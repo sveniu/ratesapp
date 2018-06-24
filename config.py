aeda47badfee4cd873523a0e44e8f53f31cc5e23
Sven Ulland <sven.ulland@tidal.com>
2018-06-24 12:55:41 +0200

    [api] Get database config from environment variables.
    Modify config.py to provide a dict-like DB object that fetches its
    values from environment variables, rather than hard-coded ones.

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
-DB = {
-    "name": "postgres",
-    "user": "postgres",
-    "host": "db"
-}
+import os
+
+class DBConfig(dict):
+    def __getitem__(self, key):
+        if key == "name":
+            return os.environ["DB_NAME"]
+        if key == "user":
+            return os.environ["DB_USER"]
+        if key == "host":
+            return os.environ["DB_HOST"]
+        raise KeyError(key)
+
+DB = DBConfig()
